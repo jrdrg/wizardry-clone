3908bd72199c76c0e99d3123b6ac16790fe148ab
jrdrg <jrodriguez.0987+github@gmail.com>
2021-04-19 16:53:35 -0400

    Updates

--- index.1619a840.js
+++ index.1619a840.js
@@ -188,12 +188,6 @@ function e(e){return e&&e.__esModule?e.default:e}var t,n,r,a,l=!1;function o(e){
       flex-direction: column;
     }
   }
-
-  @media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
-    & {
-      flex-direction: column;
-    }
-  }
 `,wm=Hh.div`
   color: white;
   flex: 1;
@@ -249,4 +243,4 @@ body,html {
   flex-direction: column;
   position: relative;
 `;function Rm(){return ge.createElement(ge.Fragment,null,ge.createElement(Tm,null),ge.createElement(zm,null,ge.createElement(Om,null,"Wizardry clone"),ge.createElement(Lm,null,ge.createElement(vm,null),ge.createElement(Nm,null))))}Xf.render(ge.createElement(Rm,null),document.getElementById("app"));
-//# sourceMappingURL=index.51ff22ee.js.map
+//# sourceMappingURL=index.1619a840.js.map
